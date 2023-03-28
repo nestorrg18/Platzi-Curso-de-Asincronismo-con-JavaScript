d2662cc5557913a2e28445956c694147470d7e62
nestorrg18 <neriga10@hotmail.com>
2023-03-27 18:21:28 -0600

    Clase 10: Callback hell

--- src/callbacks/challenge.js
+++ src/callbacks/challenge.js
@@ -33,22 +33,22 @@ fetchData(`${API}/products`, function(error1, data1){
 
 //? Otra forma de hacerlo
 // fuente: https://platzi.com/comentario/3885400/
-const XMLHttpRequest = require('XMLHttpRequest');
-const API2 = 'https://api.escuelajs.co/api/v1/products';
-const DONE = 4;
-const OK = 200;
+// const XMLHttpRequest = require('XMLHttpRequest');
+// const API2 = 'https://api.escuelajs.co/api/v1/products';
+// const DONE = 4;
+// const OK = 200;
 
-function fetchData(urlApi, callback) {
-    let xhttp = new XMLHttpRequest();
+// function fetchData(urlApi, callback) {
+//     let xhttp = new XMLHttpRequest();
 
-    xhttp.open('GET', urlApi, true);
-    xhttp.onreadystatechange = function (e) {
-        if (xhttp.readyState === DONE && xhttp.status === OK) {
-            callback(null, JSON.parse(xhttp.responseText));
-        } else {
-            const error = new Error('error' + urlApi);
-            return callback(error, null);
-        }
-    }
-    xhttp.send();
-}
+//     xhttp.open('GET', urlApi, true);
+//     xhttp.onreadystatechange = function (e) {
+//         if (xhttp.readyState === DONE && xhttp.status === OK) {
+//             callback(null, JSON.parse(xhttp.responseText));
+//         } else {
+//             const error = new Error('error' + urlApi);
+//             return callback(error, null);
+//         }
+//     }
+//     xhttp.send();
+// }
